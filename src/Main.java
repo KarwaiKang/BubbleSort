e215c049b36ef978923879a19b8ca0a3cb39b416
KarwaiKang <kkang9093@bths.edu>
2018-11-14 11:45:49 -0500

    add selectionSort

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,13 @@ public class Main {
         arr[j] = temp;
     }
 
+    public static void printArr(int[] arr) {
+        String out = "";
+        for (int i : arr)
+            out += ", " + i;
+        System.out.println(out.substring(2));
+    }
+
     public static void bubbleSort(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
@@ -14,10 +21,15 @@ public class Main {
         }
     }
 
-    public static void printArr(int[] arr) {
-        String out = "";
-        for (int i : arr)
-            out += ", " + i;
-        System.out.println(out.substring(2));
+    public static void selectionSort(int[] arr) {
+        int min = 0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                if (arr[j] < arr[min])
+                    min = j;
+            }
+            swap(arr, min, i);
+        }
+
     }
 }
--- src/Runner.java
+++ src/Runner.java
@@ -1,8 +1,8 @@
 public class Runner {
     public static void main(String[] args) {
-        int[] arr = {4,5,12,3,9,7};
+        int[] arr = {2,3,6,1,5,4};
         Main.printArr(arr);
-        Main.bubbleSort(arr);
+        Main.selectionSort(arr);
         Main.printArr(arr);
     }
 }
