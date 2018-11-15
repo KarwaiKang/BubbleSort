276053b5aa13c9b45f96fd51e79d19a82d22470e
KarwaiKang <kkang9093@bths.edu>
2018-11-15 11:25:30 -0500

    Test all sorting methods

--- src/Runner.java
+++ src/Runner.java
@@ -1,6 +1,19 @@
 public class Runner {
     public static void main(String[] args) {
+        System.out.println("Bubble Sort:");
         int[] arr = Main.getRandIntArr(25);
+        Main.printArr(arr);
+        Main.bubbleSort(arr);
+        Main.printArr(arr);
+
+        System.out.println("Selection Sort:");
+        arr = Main.getRandIntArr(25);
+        Main.printArr(arr);
+        Main.selectionSort(arr);
+        Main.printArr(arr);
+
+        System.out.println("Insertion Sort:");
+        arr = Main.getRandIntArr(25);
         Main.printArr(arr);
         Main.insertionSort(arr);
         Main.printArr(arr);
